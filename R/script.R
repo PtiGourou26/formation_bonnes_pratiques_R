d5b9a4f4d6941799d301a4e3b3929a5dc5743c6e
remialbert <remi.albert@insee.fr>
2023-02-28 14:42:00 +0000

    mis en place de renv

--- R/script.R
+++ R/script.R
@@ -1,105 +1,102 @@
-# Import des librairies ---------------------------------------------------
+# Chaine de production sur le fichier recensement diffusé par l'Insee
+
+# GESTION ENVIRONNEMENT ----------------------
 
 library(dplyr)
-library(stringr)
 library(ggplot2)
 library(forcats)
 
-
-# Import keys -------------------------------------------------------------
-api_token <- yaml::read_yaml("secrets.yaml")
+api_token <- yaml::read_yaml("secrets.yaml")$JETON_API
 
 
-# Déclaration des fonctions -----------------------------------------------
+# FONCTIONS ---------------------------------------------------------------
 
-# fonction de stat agregee
-fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
-  if (b == "moyenne") {
-    x <- mean(a, na.rm = TRUE, ...)
-  } else if (b == "ecart-type" || b == "sd") {
-    x <- sd(a, na.rm = TRUE, ...)
-  } else if (b == "variance") {
-    x <- var(a, na.rm = TRUE, ...)
-  }
-  return(x)
-}
+source("R/functions.R", encoding = "UTF-8")
 
 
-decennie_a_partir_annee <- function(annee) {
-  return(annee - annee %%
-           10)
-}
+# IMPORT DONNEES -----------------------------
 
-
-# Import des données ------------------------------------------------------
-
-df <- readr::read_csv2(
-  "individu_reg.csv",
+df <- arrow::read_parquet(
+  "individu_reg.parquet",
   col_select = c("region", "aemm", "aged", "anai", "catl", "cs1", "cs2",
-                 "cs3", "couple", "na38", "naf08", "pnai12", "sexe", "surf",
-                 "tp", "trans", "ur")
+                 "cs3", "couple", "na38", "naf08", "pnai12", "sexe",
+                 "surf", "tp", "trans", "ur")
 )
 
-
-
-# Retraitement des données ------------------------------------------------
+# RETRAITEMENT --------------------------------
 
 df <- df %>%
   mutate(aged = as.numeric(aged))
-
-# stats trans par statut
-df3 <- df %>%
-  group_by(couple, trans) %>%
-  summarise(x = n()) %>%
-  group_by(couple) %>%
-  mutate(y = 100 * x / sum(x))
 
 df$sexe <- df$sexe %>%
   as.character() %>%
   fct_recode(Homme = "1", Femme = "2")
 
 
-
-# Statistiques descriptives -----------------------------------------------
+# STATISTIQUES DESCRIPTIVES --------------------
 
 summarise(group_by(df, aged), n())
 
-fonction_de_stat_agregee(rnorm(10))
-fonction_de_stat_agregee(rnorm(10), "ecart-type")
-fonction_de_stat_agregee(rnorm(10), "variance")
+stat_agregee(df %>% filter(sexe == "Homme") %>% pull(aged))
+stat_agregee(df %>% filter(sexe == "Femme") %>% pull(aged))
 
-fonction_de_stat_agregee(df %>% filter(sexe == "Homme") %>% pull(aged))
-fonction_de_stat_agregee(df %>% filter(sexe == "Femme") %>% pull(aged))
+stats_age <- df %>%
+  group_by(decennie = decennie_a_partir_annee(aged)) %>%
+  summarise(n())
+
+table_age <- gt::gt(stats_age) %>%
+  gt::tab_header(
+    title = "Distribution des âges dans notre population"
+  ) %>%
+  gt::fmt_number(
+    columns = `n()`,
+    sep_mark = " ",
+    decimals = 0
+  ) %>%
+  gt::cols_label(
+    decennie = "Tranche d'âge",
+    `n()` = "Population"
+  )
+
+## stats trans par statut =====================
+
+df3 <- df %>%
+  group_by(couple, trans) %>%
+  summarise(x = n()) %>%
+  group_by(couple) %>%
+  mutate(y = 100 * x / sum(x))
 
 
-
-# Graphiques --------------------------------------------------------------
+# GRAPHIQUES -----------------------------------
 
 ggplot(df) +
-  geom_histogram(aes(x = 5 * floor(as.numeric(aged) / 5)), stat = "count")
+  geom_histogram(aes(x = 5 * floor(aged / 5)), stat = "count")
 
-p <- # part d'homme dans chaque cohort
-  df %>%
+# part d'homme dans chaque cohort
+p <- df %>%
   group_by(aged, sexe) %>%
   summarise(SH_sexe = n()) %>%
   group_by(aged) %>%
   mutate(SH_sexe = SH_sexe / sum(SH_sexe)) %>%
-  filter(sexe == 1) %>%
+  filter(sexe == "Homme") %>%
   ggplot() +
   geom_bar(aes(x = aged, y = SH_sexe), stat = "identity") +
-  geom_point(aes(x = aged, y = SH_sexe), stat = "identity", color = "red") +
+  geom_point(
+    aes(x = aged, y = SH_sexe),
+    stat = "identity", color = "red") +
   coord_cartesian(c(0, 100))
 
 ggsave("p.png", p)
 
 
-# Modelisation ------------------------------------------------------------
+# MODELISATION -------------------------------
 
 df3 <- df %>%
   select(surf, cs1, ur, couple, aged) %>%
   filter(surf != "Z")
-df3[, 1] <- factor(df3$surf, ordered = TRUE)
-df3[, "cs1"] <- factor(df3$cs1)
-df3 %>%
-  filter(couple == "2" & aged > 40 & aged < 60)
+
+df3 <- df3 %>%
+  mutate(surf = factor(df3$surf, ordered = TRUE),
+         cs1 = factor(cs1))
+
 MASS::polr(surf ~ cs1 + factor(ur), df3)
